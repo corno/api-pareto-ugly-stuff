c014337a13c47268feaa925d303c62f9c15d1542
corno <corno.astn.a@schraverus.com>
2022-08-30 10:10:52 +0200

    added 'Max'

--- pub/src/interface/tbd.ts
+++ pub/src/interface/tbd.ts
@@ -1,5 +1,6 @@
 
 import * as pt from "pareto-core-types"
+import { Nested } from "pareto-core-types"
 
 export type ArrayLength = <T>(array: T[]) => number
 export type GetElement = <T>(array: T[], position: number) => T
@@ -8,8 +9,16 @@ export type Includes = (array: string[], value: string) => boolean
 
 export type JSONStringify = (data: any) => string
 export type JSONParse = (str: string) => any
+
+
 export type ToRawArray = <T>($: pt.Array<T>) => T[]
 export type ToRawDictionary = <T>($: pt.Dictionary<T>) => { [key: string]: T }
+
+//string
 export type TrimEnd = (value: string) => string
 export type StringLength = (value: string) => number
 export type SubStr = (value: string, begin: number, length: number) => string
+
+export type Max = (a: number, b: Nested<number>) => number
+
+
