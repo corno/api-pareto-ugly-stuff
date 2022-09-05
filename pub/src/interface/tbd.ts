6a223e2235a1a31cb11a11909de5d9ea7a702655
corno <corno.astn.a@schraverus.com>
2022-09-05 07:27:30 +0200

    .

--- pub/src/interface/tbd.ts
+++ pub/src/interface/tbd.ts
@@ -1,25 +1,37 @@
 
-import * as pt from "pareto-core-types"
 import * as pm from "pareto-core-state"
+import * as pt from "pareto-core-types"
 
-export type FArrayLength = <T>(array: T[]) => number
-export type FGetElement = <T>(array: T[], position: number) => T
-export type XPush = <T>(array: T[], element: T) => void
-export type FIncludes = (array: string[], value: string) => boolean
+export type FArrayLength = <T>($: pt.Array<T>) => number
+export type FGetElement = <T>($: {
+    readonly "array": pt.Array<T>,
+    readonly "position": number
+}) => T
+export type FIncludes = ($: {
+    readonly "array": pt.Array<string>,
+    readonly "value": string
+}) => boolean
 
-export type FJSONStringify = <T>(data: T) => string
-export type FJSONParse = <T>(str: string) => T
+export type FJSONStringify = <T>($: T) => string
+export type FJSONParse = <T>($: string) => T
 
 
-export type FToRawArray = <T>($: pt.Array<T>) => T[]
-export type FToRawDictionary = <T>($: pt.Dictionary<T>) => { [key: string]: T }
+//export type XPush = <T>(array: T[], element: T) => void
+//export type FToRawArray = <T>($: pt.Array<T>) => T[]
+//export type FToRawDictionary = <T>($: pt.Dictionary<T>) => { [key: string]: T }
 
 //string
-export type FTrimEnd = (value: string) => string
-export type FStringLength = (value: string) => number
-export type FSubStr = (value: string, begin: number, length: number) => string
-
-export type FMax = (a: number, b: pt.Nested<number>) => number
+export type FTrimEnd = ($: string) => string
+export type FStringLength = ($: string) => number
+export type FSubStr = ($: {
+    readonly "value": string,
+    readonly "begin": number,
+    readonly "length": number,
+}) => string
+export type FStartsWith = ($: {
+    contextString: string
+    searchString: string
+}) => boolean
 
 export type XDoUntil = <T>(stack: pm.Stack<T>, callback: ($: T) => boolean) => void
 export type XLookAhead = <T>(stack: pm.Stack<T>, exists: ($: T) => void, notExists: () => void) => void
